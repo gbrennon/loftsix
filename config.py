63ea2d73cdac98673164173e5e453e32f0d0c2f2
Rafael Teixeira <p51801407@brasilprev.com.br>
2019-12-01 00:51:59 -0300

    Config file now works on heroku

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://localhost:5432/loftsix')
+
+if os.getenv('PROD', True):
+    DATABASE_URL = os.getenv(
+        'DATABASE_URL', 'postgres://localhost:5432/loftsix')
+else:
+    db = 'loftsix'
+    port = os.getenv('DB_PORT', 5433)
+    user = os.getenv('DB_USER', 'postgres')
+    pw = os.getenv('DB_PASSWORD', 'postgres')
+    host = os.getenv('DB_HOST', 'localhost')
+    DATABASE_URL = f'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'
