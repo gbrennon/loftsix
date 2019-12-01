390d626a1027b149b3433a5b1ff6d008afa2a47e
Glauber Brennon <glauberbrennon@gmail.com>
2019-12-01 05:54:02 -0300

    Added survey to routes

--- app/routes.py
+++ app/routes.py
@@ -11,4 +11,8 @@ bp = Blueprint('', __name__, url_prefix='')
 def index():
     properties = get_all()
     print(properties)
-    return render_template('index.html')
+    return render_template('home.html')
+
+@bp.route('/survey')
+def survey():
+    return render_template('survey.html')
